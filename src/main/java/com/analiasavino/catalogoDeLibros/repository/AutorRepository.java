af338aa97ccec53f325981f00a6bca1a08cb8d43
Analia Savino <anuchetita@gmail.com>
2024-07-06 01:01:47 -0300

    Estan todas las funcionalidades pedidas funcionando

--- src/main/java/com/analiasavino/catalogoDeLibros/repository/AutorRepository.java
+++ src/main/java/com/analiasavino/catalogoDeLibros/repository/AutorRepository.java
@@ -11,6 +11,8 @@ public interface AutorRepository extends JpaRepository<Autor, Long> {
 
     Optional<Autor> findByNombre(String nombre);
 
+    @Query("SELECT a FROM Autor a WHERE a.fechaDeNacimiento <= :anio AND a.fechaDeFallecimiento >= :anio")
+    List<Autor> findAutoresVivosSegunAnio(Integer anio);
 
 
 }
--- src/main/java/com/analiasavino/catalogoDeLibros/repository/LibroRepository.java
+++ src/main/java/com/analiasavino/catalogoDeLibros/repository/LibroRepository.java
@@ -3,13 +3,17 @@ package com.analiasavino.catalogoDeLibros.repository;
 import com.analiasavino.catalogoDeLibros.model.Idioma;
 import com.analiasavino.catalogoDeLibros.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface LibroRepository extends JpaRepository<Libro, Long>{
+public interface LibroRepository extends JpaRepository<Libro, Long> {
 
   List<Libro> findByIdioma(Idioma idioma);
 
   Optional<Libro> findByTitulo(String titulo);
+
+// @Query("SELECT l from Libro l ORDER BY l.numeroDeDecargas DESC LIMIT 10")
+ List<Libro> findTop10ByOrderByNumeroDeDescargasDesc();
 }
